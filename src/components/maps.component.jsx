f1c75533e204d960ce1d7ee632785fa8a25651d6
Dinesh <dineshkumar.software@gmail.com>
2023-12-07 21:58:21 +0530

    Search Restaurants

--- src/components/maps.component.jsx
+++ src/components/maps.component.jsx
@@ -1,55 +1,109 @@
 import H from '@here/maps-api-for-javascript';
-import { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+import { Fragment, useEffect, useRef, useState } from 'react';
+import RestIcon from "../assets/restaurant-icon.svg";
+import { motion } from "framer-motion";
 
 export default function MapElement({ latitude, longitude }) {
     const mapRef = useRef(null);
     const map = useRef(null);
-    const platform = useRef(null)
+    const platform = useRef(null);
+
+    const restaurantGroup = useRef(null);
+
     const apikey = "L0-2LCY4n1A2kedFrMAlPVsd9bjfFL9RmaA-JTv-Sgg";
+
+    const [restaurants, setRestaurants] = useState();
+
+    function searchArea() {
+        const { lat, lng } = map.current.getCenter()
+        axios.get(`https://browse.search.hereapi.com/v1/browse?at=${lat},${lng}&limit=100&categories=100-1000-0000&apiKey=${apikey}`).then((res) => {
+
+            setRestaurants((x) => {
+                if (x) {
+                    var tempDict = { ...x }
+                    res.data.items.map((item) => {
+                        tempDict[item.id] = item;
+                    })
+                    return tempDict;
+                }
+                var newDict = {};
+                res.data.items.map((item) => {
+                    newDict[item.id] = item;
+                })
+                return newDict;
+            });
+        })
+    }
+
+    function clearRestaurants() {
+        setRestaurants();
+        console.log(restaurantGroup.current)
+        map.current.removeObject(restaurantGroup.current);
+        restaurantGroup.current = new H.map.Group();
+        map.current.addObject(restaurantGroup.current);
+    }
 
     useEffect(
         () => {
             if (!latitude) return;
-            // Check if the map object has already been created
+
             if (!map.current) {
-                // Create a platform object with the API key
                 platform.current = new H.service.Platform({ apikey });
-                // Create a new Raster Tile service instance
-                const rasterTileService = platform.current.getRasterTileService({
-                    queryParams: {
-                        style: "explore.night",
-                        size: 512,
-                    },
-                });
-                // Creates a new instance of the H.service.rasterTile.Provider class
-                // The class provides raster tiles for a given tile layer ID and pixel format
-                const rasterTileProvider = new H.service.rasterTile.Provider(
-                    rasterTileService
-                );
-                // Create a new Tile layer with the Raster Tile provider
-                const rasterTileLayer = new H.map.layer.TileLayer(rasterTileProvider);
-                // Create a new map instance with the Tile layer, center and zoom level
-                const newMap = new H.Map(mapRef.current, rasterTileLayer, {
-                    pixelRatio: window.devicePixelRatio,
-                    center: {
-                        lat: latitude,
-                        lng: longitude,
-                    },
-                    zoom: 14,
+                var defaultLayers = platform.current.createDefaultLayers();
+
+                const newMap = new H.Map(mapRef.current, defaultLayers.vector.normal.map, {
+                    center: { lat: latitude, lng: longitude },
+                    zoom: 16,
+                    pixelRatio: window.devicePixelRatio || 1
                 });
 
-                // Add panning and zooming behavior to the map
+                var currentLocation = new H.map.Circle({ lat: latitude, lng: longitude }, 10);
+                newMap.addObject(currentLocation);
+                var currentLocation = new H.map.Circle({ lat: latitude, lng: longitude }, 125);
+
+                newMap.addObject(currentLocation);
+
+                restaurantGroup.current = new H.map.Group();
+
+                newMap.addObject(restaurantGroup.current);
+
                 const behavior = new H.mapevents.Behavior(
                     new H.mapevents.MapEvents(newMap)
                 );
 
-                // Set the map object to the reference
                 map.current = newMap;
             }
         },
-        // Dependencies array
-        [apikey, location]
-    );
 
-    return <div style={{ width: "100%", height: "80vh" }} ref={mapRef} />
+        [apikey, latitude, longitude]
+    );
+    useEffect(() => {
+        if (!restaurants) return;
+        var icon = new H.map.Icon(RestIcon);
+
+        for (const [key, value] of Object.entries(restaurants)) {
+            restaurantGroup.current.addObject(new H.map.Marker({ lat: value.position.lat, lng: value.position.lng }, { icon: icon }))
+        }
+
+    }, [restaurants])
+    return (
+        <Fragment>
+            <motion.div
+                className="absolute flex select-none cursor-pointer z-10 gap-2">
+                <motion.span className="bg-black p-3 rounded-xl mt-4" onClick={searchArea} whileHover={{
+                    scale: 1.05,
+                    transition: { duration: .3 },
+                }}
+                    whileTap={{ scale: 0.9 }}>Search This Area</motion.span>
+                <motion.span whileHover={{
+                    scale: 1.05,
+                    transition: { duration: .3 },
+                }}
+                    whileTap={{ scale: 0.9 }}
+                    className="bg-black p-3 rounded-xl mt-4" onClick={clearRestaurants}>X</motion.span>
+            </motion.div>
+            <div className='absolute' style={{ width: "100%", height: "80vh" }} ref={mapRef} />
+        </Fragment>
+    )
 }
