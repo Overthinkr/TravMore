5fb9fe5fcae842381196ddd9eecf3ae27caf5b8d
Mayank Roy <roy050703@gmail.com>
2023-12-09 00:50:36 +0530

    Invest page prototype completed

--- src/components/invest/investcards.component.jsx
+++ src/components/invest/investcards.component.jsx
@@ -1,14 +1,22 @@
+import { useState } from "react";
+
 export default function Investcard({ location, country, description }) {
   return (
-    <div className="flex flex-row">
-      <div className="flex flex-col">
-        <h1 className="text-xl font-bold">{location}</h1>
-        <p className="text-md">{country}</p>
+    <>
+      <div className="flex flex-row w-full justify-between text-center items-center border border-gray-600 rounded-2xl">
+        <div className="flex flex-col mx-6">
+          <h1 className="text-xl font-bold">{location}</h1>
+          <p className="text-md">{country}</p>
+        </div>
+        <div className="flex flex-col">
+          <button className="bg-purple-800 rounded-lg p-2 m-2 cursor-not-allowed">
+            Know more
+          </button>
+          <button className="bg-purple-800 rounded-lg p-2 m-2 cursor-not-allowed">
+            Invest
+          </button>
+        </div>
       </div>
-      <div className="flex flex-col">
-        <button className="bg-purple-800 rounded-lg p-2 m-2">Know more</button>
-        <button className="bg-purple-800 rounded-lg p-2 m-2">Invest</button>
-      </div>
-    </div>
+    </>
   );
 }
