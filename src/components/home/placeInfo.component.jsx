a7b9814ee931c1d9491b10aa3817956f4a36341a
Dinesh <dineshkumar.software@gmail.com>
2023-12-09 19:27:33 +0530

    Home complete

--- src/components/home/placeInfo.component.jsx
+++ src/components/home/placeInfo.component.jsx
@@ -1,10 +1,13 @@
-import { Fragment, useState } from 'react';
+import { Fragment, useEffect, useRef, useState } from 'react';
 import { Rating } from 'react-simple-star-rating';
 import { calculateDistance } from '../../helpers/BrowseAPI.helper';
 import { motion } from "framer-motion";
+import axios from 'axios';
 
 export default function PlaceInfo({ info, latitude, longitude, setDetailsModal, setDisplayModal, setShowResults, findDirections }) {
     const [rating, setRating] = useState(0)
+
+    const reviewRef = useRef();
 
     const handleRating = (rate) => {
         setRating(rate)
@@ -17,6 +20,44 @@ export default function PlaceInfo({ info, latitude, longitude, setDetailsModal, 
     const [overall, setOverall] = useState(0);
     const [parking, setParking] = useState(0);
 
+    const [ratingSubmit, setRatingSubmit] = useState(0);
+    const [reviewSubmit, setReviewSubmit] = useState(0);
+
+    const [currentRating, setCurrentRating] = useState();
+
+    const [currentReviews, setCurrentReviews] = useState([]);
+
+    useEffect(() => {
+        axios.get(`https://travmoreapi.up.railway.app/get_rating?placeID=${info.id}`).then((res) => {
+            setCurrentRating(res.data?.rating)
+        })
+        axios.get(`https://travmoreapi.up.railway.app/get_reviews?placeID=${info.id}`).then((res) => {
+            setCurrentReviews(res.data)
+        })
+    }, [])
+
+    console.log(currentReviews)
+
+    function submitRating() {
+        if (rating === 0) return;
+        setRatingSubmit(1);
+        axios.post(`https://travmoreapi.up.railway.app/add_rating?rating=${rating}&placeID=${info.id}`).catch(() => {
+            setRatingSubmit(3)
+        }).finally(() => {
+            setRatingSubmit(2);
+        })
+    }
+
+    function submitReview() {
+        if (overall === 0 || parking === 0 || ambience === 0 || environment === 0 || reviewRef.current.value === null) return;
+        setReviewSubmit(1)
+        axios.post(`https://travmoreapi.up.railway.app/add_review?placeID=${info.id}&OverallRating=${overall}&ParkingRating=${parking}&AmbienceRating=${ambience}&TreatmentRating=${environment}&review_text=${reviewRef.current.value}`).catch(() => {
+            setReviewSubmit(3);
+        }).finally(() => {
+            setReviewSubmit(2)
+            setReview(false)
+        })
+    }
 
     if (!info) return;
 
@@ -32,7 +73,7 @@ export default function PlaceInfo({ info, latitude, longitude, setDetailsModal, 
                         :
                         <span className='bg-red-500 p-1 px-2 rounded-lg text-xs'>Closed</span>
                     }
-                    <div className="flex text-xs px-2 py-1 border-red-500 border rounded-md items-center">5.0</div>
+                    <div className="flex text-xs px-2 py-1 border-red-500 border rounded-md items-center">{currentRating ? Math.round(currentRating * 100) / 100 : "No ratings available"}</div>
                     <motion.span whileHover={{
                         scale: 1.05,
                         transition: { duration: .3 },
@@ -59,9 +100,14 @@ export default function PlaceInfo({ info, latitude, longitude, setDetailsModal, 
             {
                 review ?
                     <div className="flex flex-col gap-4 text-sm items-center">
-                        <div className="flex text-base justify-center py-2">
-                            Add a review
-                        </div>
+                        {
+                            reviewSubmit !== 2 ?
+                                <div className="flex text-base justify-center py-2">
+                                    Add a review
+                                </div>
+                                : ""
+                        }
+
                         <div className="flex gap-1 items-center">
                             Overall
                             <Rating iconsCount={5} size={25} SVGclassName={"inline-block"}
@@ -91,11 +137,23 @@ export default function PlaceInfo({ info, latitude, longitude, setDetailsModal, 
                                 }} />
                         </div>
 
-                        <textarea className='w-full p-2 rounded-md' rows="3" placeholder='Add a review...' />
+                        <textarea className='w-full p-2 rounded-md' rows="3" placeholder='Add a review...' ref={reviewRef} />
 
                         <div className="flex gap-2">
                             <button className='bg-red-500' onClick={() => { setReview(false) }}>Cancel Review</button>
-                            <button>Submit Review</button>
+                            <button onClick={submitReview}>
+                                {
+                                    reviewSubmit === 0 && "Submit Review"
+                                }
+                                {
+                                    reviewSubmit === 1 && "Submitting Review"
+                                }
+                                {
+                                    reviewSubmit === 2 && "Submitted Review"
+                                }
+                                {
+                                    reviewSubmit === 3 && "Failed Submission"
+                                }</button>
                         </div>
 
                     </div>
@@ -148,7 +206,18 @@ export default function PlaceInfo({ info, latitude, longitude, setDetailsModal, 
                         <div className="flex flex-col justify-center items-center gap-2 py-3 text-xs">
                             <Rating iconsCount={5} size={25} SVGclassName={"inline-block"}
                                 onClick={handleRating} />
-                            <button className="text-xs w-fit">Submit Rating</button>
+                            <button className="text-xs w-fit" onClick={submitRating}>{
+                                ratingSubmit === 0 && "Submit Rating"
+                            }
+                                {
+                                    ratingSubmit === 1 && "Uploading Rating"
+                                }
+                                {
+                                    ratingSubmit === 2 && "Submitted Rating"
+                                }
+                                {
+                                    ratingSubmit === 3 && "Failed Submition"
+                                }</button>
                         </div>
 
                         <div className="flex justify-center items-center">
@@ -160,16 +229,34 @@ export default function PlaceInfo({ info, latitude, longitude, setDetailsModal, 
                         </div>
 
                         <div className="flex flex-col text-sm py-2 gap-2">
-                            <div className="flex w-full justify-between">
-                                Username
-                                <div className="flex">
-                                    <div className="flex text-xs px-2 py-1 border-red-500 border rounded-md items-center">5.0</div>
-                                </div>
-                            </div>
+                            {
+                                currentReviews.length === 0 ?
 
-                            <div className="flex text-xs">
-                                Review
-                            </div>
+                                    "No reviews yet" :
+                                    currentReviews.map((review) => {
+                                        return <div className="flex flex-col">
+                                            <div className="flex w-full justify-between">
+                                                {review.UserID}
+                                                <div className="flex text-xs items-center gap-2">
+                                                    <div className="flex  px-2 py-1 border-red-500 border rounded-md items-center gap-1"><span className="material-symbols-outlined text-xs">star</span>{review.OverallRating}</div>
+                                                    <div className="flex px-2 py-1 border-red-500 border rounded-md">
+                                                        Ambience: {review.AmbienceRating}
+                                                    </div>
+                                                    <div className="flex px-2 py-1 border-red-500 border rounded-md">
+                                                        Treatment: {review.TreatmentRating}
+                                                    </div>
+                                                    <div className="flex px-2 py-1 border-red-500 border rounded-md">
+                                                        Parking: {review.ParkingRating}
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div className="flex text-sm py-2">
+                                                {review.ReviewText}
+                                            </div>
+                                        </div>
+                                    })
+                            }
                         </div>
                     </Fragment>
             }
